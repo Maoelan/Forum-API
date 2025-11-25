1e515e16ff928140b91b24d242ace99072f2fbef
Maulana Muhammad <maulanamuhammad2000@gmail.com>
2025-11-25 20:02:25 +0700

    refactor codebase to comply with ESLint standards

--- src/Domains/replies/ReplyRepository.js
+++ src/Domains/replies/ReplyRepository.js
@@ -1,9 +1,9 @@
 class ReplyRepository {
-  async addReply(ownerId, commentId, newReply) { throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED'); }
-  async verifyReplyOwner(replyId, ownerId) { throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED'); }
-  async checkReplyExists(replyId, commentId) { throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED'); }
-  async deleteReply(replyId) { throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED'); }
-  async getRepliesByCommentIds(commentIds) { throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED'); }
+  async addReply() { throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED'); }
+  async verifyReplyOwner() { throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED'); }
+  async checkReplyExists() { throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED'); }
+  async deleteReply() { throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED'); }
+  async getRepliesByCommentIds() { throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED'); }
 }
 
 module.exports = ReplyRepository;
