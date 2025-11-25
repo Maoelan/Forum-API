de62480de870bd2d63d87aa7da383e9653af5564
Maulana Muhammad <maulanamuhammad2000@gmail.com>
2025-11-25 18:20:30 +0700

    refactor Application/use_case

--- src/Applications/use_case/_test/AddReplyUseCase.test.js
+++ src/Applications/use_case/_test/AddReplyUseCase.test.js
@@ -4,31 +4,15 @@ const NewReply = require('../../../Domains/replies/entities/NewReply');
 describe('AddReplyUseCase', () => {
   it('should orchestrate the add reply action correctly', async () => {
     // Arrange
-    const payload = {
-      threadId: 'thread-123',
-      commentId: 'comment-123',
-      content: 'ini balasan dari user',
-    };
+    const payload = { threadId: 'thread-123', commentId: 'comment-123', content: 'ini balasan dari user' };
     const ownerId = 'user-123';
 
-    const expectedAddedReply = {
-      id: 'reply-123',
-      content: payload.content,
-      owner: ownerId,
-    };
+    const expectedAddedReply = { id: 'reply-123', content: payload.content, owner: ownerId };
 
-    /** create mock repositories */
-    const mockThreadRepository = {
-      verifyThreadExists: jest.fn().mockResolvedValue(),
-    };
-    const mockCommentRepository = {
-      checkCommentExists: jest.fn().mockResolvedValue(),
-    };
-    const mockReplyRepository = {
-      addReply: jest.fn().mockResolvedValue(expectedAddedReply),
-    };
+    const mockThreadRepository = { verifyThreadExists: jest.fn().mockResolvedValue() };
+    const mockCommentRepository = { checkCommentExists: jest.fn().mockResolvedValue() };
+    const mockReplyRepository = { addReply: jest.fn().mockResolvedValue(expectedAddedReply) };
 
-    /** create use case instance */
     const addReplyUseCase = new AddReplyUseCase({
       threadRepository: mockThreadRepository,
       commentRepository: mockCommentRepository,
@@ -39,12 +23,9 @@ describe('AddReplyUseCase', () => {
     const result = await addReplyUseCase.execute(ownerId, payload);
 
     // Assert
-    // memastikan NewReply dibuat
     expect(result).toEqual(expectedAddedReply);
-    expect(mockThreadRepository.verifyThreadExists)
-      .toHaveBeenCalledWith(payload.threadId);
-    expect(mockCommentRepository.checkCommentExists)
-      .toHaveBeenCalledWith(payload.commentId, payload.threadId);
+    expect(mockThreadRepository.verifyThreadExists).toHaveBeenCalledWith(payload.threadId);
+    expect(mockCommentRepository.checkCommentExists).toHaveBeenCalledWith(payload.commentId, payload.threadId);
     expect(mockReplyRepository.addReply)
       .toHaveBeenCalledWith(ownerId, payload.commentId, new NewReply({ content: payload.content }));
   });
--- src/Applications/use_case/_test/AddUserUseCase.test.js
+++ src/Applications/use_case/_test/AddUserUseCase.test.js
@@ -5,57 +5,39 @@ const PasswordHash = require('../../security/PasswordHash');
 const AddUserUseCase = require('../AddUserUseCase');
 
 describe('AddUserUseCase', () => {
-  /**
-   * Menguji apakah use case mampu mengoskestrasikan langkah demi langkah dengan benar.
-   */
-  it('should orchestrating the add user action correctly', async () => {
+  it('should orchestrate the add user action correctly', async () => {
     // Arrange
-    const useCasePayload = {
-      username: 'dicoding',
-      password: 'secret',
-      fullname: 'Dicoding Indonesia',
-    };
-
+    const useCasePayload = { username: 'dicoding', password: 'secret', fullname: 'Dicoding Indonesia' };
     const mockRegisteredUser = new RegisteredUser({
       id: 'user-123',
       username: useCasePayload.username,
       fullname: useCasePayload.fullname,
     });
 
-    /** creating dependency of use case */
     const mockUserRepository = new UserRepository();
     const mockPasswordHash = new PasswordHash();
 
-    /** mocking needed function */
-    mockUserRepository.verifyAvailableUsername = jest.fn()
-      .mockImplementation(() => Promise.resolve());
-    mockPasswordHash.hash = jest.fn()
-      .mockImplementation(() => Promise.resolve('encrypted_password'));
-    mockUserRepository.addUser = jest.fn()
-      .mockImplementation(() => Promise.resolve(mockRegisteredUser));
+    mockUserRepository.verifyAvailableUsername = jest.fn().mockResolvedValue();
+    mockPasswordHash.hash = jest.fn().mockResolvedValue('encrypted_password');
+    mockUserRepository.addUser = jest.fn().mockResolvedValue(mockRegisteredUser);
 
-    /** creating use case instance */
-    const getUserUseCase = new AddUserUseCase({
+    const addUserUseCase = new AddUserUseCase({
       userRepository: mockUserRepository,
       passwordHash: mockPasswordHash,
     });
 
-    // Action
-    const registeredUser = await getUserUseCase.execute(useCasePayload);
+    // Act
+    const registeredUser = await addUserUseCase.execute(useCasePayload);
 
     // Assert
-    expect(registeredUser).toStrictEqual(new RegisteredUser({
-      id: 'user-123',
-      username: useCasePayload.username,
-      fullname: useCasePayload.fullname,
-    }));
-
+    expect(registeredUser).toStrictEqual(mockRegisteredUser);
     expect(mockUserRepository.verifyAvailableUsername).toHaveBeenCalledWith(useCasePayload.username);
     expect(mockPasswordHash.hash).toHaveBeenCalledWith(useCasePayload.password);
-    expect(mockUserRepository.addUser).toHaveBeenCalledWith(new RegisterUser({
-      username: useCasePayload.username,
-      password: 'encrypted_password',
-      fullname: useCasePayload.fullname,
-    }));
+    expect(mockUserRepository.addUser)
+      .toHaveBeenCalledWith(new RegisterUser({
+        username: useCasePayload.username,
+        password: 'encrypted_password',
+        fullname: useCasePayload.fullname,
+      }));
   });
 });
